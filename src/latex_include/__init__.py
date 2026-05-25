6ba2db198ed88bdd4ee6c5293d43cb9466b62fc2
bjhend <developer@bjhend.de>
2026-05-25 13:07:41 +0200

    Fix: package functions not exported

--- src/latex_include/__init__.py
+++ src/latex_include/__init__.py
@@ -2,7 +2,9 @@
 # Dynamic type checking with beartype
 from beartype.claw import beartype_this_package
 beartype_this_package()
+# Don't export beartype_this_package
+del beartype_this_package
 
 
-from .main import main
+from .main import latexIncludeFiles, latexInclude
 
